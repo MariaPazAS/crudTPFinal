006d198dfc09905f7a9206749a455f1ac99368df
MariaPazAS <as.mariapaz@gmail.com>
2023-07-04 11:40:15 -0300

    Subiendo archivos

---
+++ front/js/productos.js
@@ -0,0 +1,47 @@
+const {createApp} = Vue
+
+createApp({
+    data(){
+        return{
+           productos: [],
+           url: 'https://mariapaz.pythonanywhere.com/productos', 
+           cargando: true,
+           error: false
+        }
+    },
+
+    methods:{
+        fetchData(url){
+            fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.productos = data;
+                this.cargando = false;
+            })
+            .catch(err => {
+                console.error(err);
+                this.error = true;
+            })
+        },
+
+        eliminar(producto) {
+            const url = 'https://mariapaz.pythonanywhere.com/productos/'+producto;
+            let options = {
+                method: 'DELETE'
+            }
+            fetch(url, options)
+            .then(res => res.json())
+            .then(res => {
+                    location.reload();
+            })
+            .catch(err => {
+                console.error(err);
+                this.error = true;
+            })
+        }
+    },
+
+    created(){
+        this.fetchData(this.url);
+    }
+}).mount('#app')
